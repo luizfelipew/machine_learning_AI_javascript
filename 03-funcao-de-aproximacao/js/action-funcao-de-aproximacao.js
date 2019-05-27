72406b3ab98f02bb1cbd79c24ba58e56ba72c612
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-05-26 21:00:46 -0300

    Funcao de aproximacao

---
+++ 03-funcao-de-aproximacao/js/action-funcao-de-aproximacao.js
@@ -0,0 +1,51 @@
+﻿function derivada() {
+	return n * (1 - n);
+}
+
+function executar(){
+	$('#linhas').html('');
+
+	var input = $('#entrada').val();
+	var target = $('#busca').val();
+	var weight = Math.random();
+	var epochs = $('#epocas').val();
+
+	var lines = '';
+
+	for(let i=0; i<=epochs; i++){
+		var output = Math.tanh(input * weight);
+		var error = target - output;
+		weight += input * derivada(error);
+
+		lines += 
+		"<tr>" + 
+			"<td>" + i + "</td>" + 
+			"<td>" + zeros(Math.abs(error.toFixed(4))) + "</td>" + 
+			"<td>" + output.toFixed(8) + "</td>" + 
+		"</tr>"
+	}
+
+	$('#linhas').html(lines);
+
+	$('#desc_entrada').text('entrada: ' + input);
+	$('#desc_busca').text('busca: ' + target);
+	$('#desc_epocas').text('epocas: ' + epochs);
+
+}
+
+function zeros(n) {
+	if(n == 0) {
+		return '0.0000';
+	}else {
+		var str = n.toString().trim();
+		var length = str.length;
+		var dif = 6 - length;
+		if(dif > 0) {
+			for(let i=0; i<dif; i++) {
+				str += '0';
+			}
+		}
+		n = str;
+		return n;
+	}
+}
